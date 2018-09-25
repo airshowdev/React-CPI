ffa30600e39df98544897bb8212258e888dcbca9
daltonedmisten@gmail.com <Ed boy@595.SCS>
2018-09-25 10:16:22 -0500

    Added CPI Client

--- CPI.Client/Program.cs
+++ CPI.Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CPI
+namespace CPI.Client
 {
     public class Program
     {
