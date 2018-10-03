4eb806b60d2601f82a91671549606bf630b65195
Nicholas J. Hall <nicholas.hall@595.SCS>
2018-10-03 10:30:14 -0500

    Added more UI Pages

---
+++ CPI.Client/ClientApp/src/components/AnalyzeData.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+import './css/uswds.css';
+import querystring from 'query-string';
+import PropTypes from 'prop-types';
+
+export class AnalyzeData extends Component {
+
+    displayName = AnalyzeData.name;
+
+    constructor(props, context) {
+        super(props, context)
+        this.state = { project: {}, loading: true };
+
+
+    }
+
+    render(project) {
+        return ( 
+            <div>
+                <h1> Analyze Data </h1>
+
+
+            </div>
+        )
+    }    
+}
