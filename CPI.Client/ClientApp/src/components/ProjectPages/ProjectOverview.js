ceaf587c7882bd2c037c010c48d6da74fab2d9ed
Gabriel P. Stines <PagChomp@595.SCS>
2018-10-22 11:09:48 -0500

    navigation stuff

--- CPI.Client/ClientApp/src/components/ProjectPages/ProjectOverview.js
+++ CPI.Client/ClientApp/src/components/ProjectPages/ProjectOverview.js
@@ -3,7 +3,8 @@ import '../css/uswds.css';
 import querystring from 'query-string';
 import PropTypes from 'prop-types';
 import '../css/HallMartino.css';
-import { NavButtons } from '../NavButtons'
+import { NavButtons } from '../NavButtons';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export class ProjectOverview extends Component {
 
@@ -27,61 +28,61 @@ export class ProjectOverview extends Component {
     }
 
     render() {
-		return (
-			<div>
-				<NavButtons next="DataCollection" previous="ProjectInfo" projectId={this.props.match.params.id}/>
-            <div className="usa-grid">
-                <div className="grid-with-border">
-                    <h1 className="usa-heading">Pre-Event Planning Overview</h1>
-                        <h2>T-3 Weeks</h2>
-                            <button className="usa-button">Initiate Data Collection</button>
-                            <button className="usa-button">Event Go/No Go Decision</button>
-                            <button className="usa-button">Request Mentor</button>
-                            <button className="usa-button">Meet With Champion</button>
-                            <button className="usa-button">Meet with Team Leader</button>
-                            <button className="usa-button">Draft Charter</button>
-                </div>
-                <div className="grid-with-border">
-                    <div className="grid-with-border">
-                    <h1>Pre-Event Preperation Overview</h1>                    
-                    <h2>T-2 Weeks</h2>
-                            <button className="usa-button">Process Walk</button>
-                            <button className="usa-button">Finalize Charter</button>
-                            <button className="usa-button">Review KPI</button>
-                            <button className="usa-button">Identify Event Location</button>
-                            <button className="usa-button">Distribute Event Notification and Charter</button>
-                            <button className="usa-button">Schedule In/Brief Out Brief</button>
-                        </div>
-                        <h2>T-1 Weeks</h2>
-                            <button className="usa-button">Team Process Walk</button>
-                            <button className="usa-button">Team Kick-Off and Awareness Training</button>
-                            <button className="usa-button">Validate Data Collection</button>
-                            <button className="usa-button">Obtain Supplies</button>
-                            <button className="usa-button">Event Go/No-Go</button>
-                            <button className="usa-button">Room Set-Up</button>
-                </div>
-                <div className="grid-with-border">
-                    <h1 className="usa-heading">Event Execution Overview</h1>
-                        <h2>T-0 Weeks</h2>
-                            <button className="usa-button">Clarify and Validate the Problem</button>
-                            <button className="usa-button">Identify Performance Gaps</button>
-                            <button className="usa-button">Set Improvement Target</button>
-                            <button className="usa-button">Determine Root Cause(s)</button>
-                            <button className="usa-button">Develop Countermeasures</button>
-                </div>
-                <div className="grid-with-border">
-                    <h1 className="usa-heading">Post-Event Implementation Overview</h1>
-                        <h2>T+90 Days</h2>
-                            <button className="usa-button">See Countermeasures Through</button>
-                            <button className="usa-button">Validate Results</button>
-                </div>
-                <div className="grid-with-border">
-                    <h1 className="usa-heading">Post-Event Follow Up Overview</h1>
-                        <h2>T+120 Days</h2>
-                            <button className="usa-button">Standardize Successful Process</button>
-                </div>
-				</div>
-				</div>
+        return (
+            <div>
+                <NavButtons next="DataCollection" previous="ProjectInfo" projectId={this.props.match.params.id} title="Project Overview" />
+            <table style={{width: '85%'}}>
+                <thead>
+                    <th>Pre-Event Planning</th>
+                    <th>Pre-Event Preparation</th>
+                    <th>Pre-Event Preparation</th>
+                    <th>Event Execution</th>
+                    <th>Post-Event-Implementation</th>
+                    <th>Post-Event Follow Up</th>
+                    </thead>
+                <tbody>
+                    <td>
+                        <LinkContainer to={"/Project/DataCollection/" + this.props.match.params.id} ><p>Initiate Data Collection</p></LinkContainer>
+                        <LinkContainer to={"/Project/AnalyzeData/" + this.props.match.params.id}><p>Event Go/No Go Decision</p></LinkContainer>
+                        <LinkContainer to={"/Project/RequestMentor/" + this.props.match.params.id}><p>Request Mentor</p></LinkContainer>
+                        <LinkContainer to={"/Project/MeetWithChampion/" + this.props.match.params.id}><p>Meet With Champion</p></LinkContainer>
+                        <LinkContainer to={"/Project/MeetwithTeamLeader/" + this.props.match.params.id}><p>Meet with Team Leader</p></LinkContainer>
+                        <LinkContainer to={"/Project/DraftCharter/" + this.props.match.params.id}><p>Draft Charter</p></LinkContainer>
+                    </td>
+                    <td>
+                    <LinkContainer to={"/Project/Process/" + this.props.match.params.id}><p>Process Walk</p></LinkContainer>
+                    <LinkContainer to={"/Project/FinalizeCharter/" + this.props.match.params.id}><p>Finalize Charter</p></LinkContainer>
+                    <LinkContainer to={"/Project/ReviewKPI/" + this.props.match.params.id}><p>Review KPI</p></LinkContainer>
+                    <LinkContainer to={"/Project/IdentifyEventLocation/" + this.props.match.params.id}><p>Identify Event Location</p></LinkContainer>
+                    <LinkContainer to={"/Project/DistributeEventNotification/" + this.props.match.params.id}><p>Distribute Event Notification and Charter</p></LinkContainer>
+                    <LinkContainer to={"/Project/ScheduleInOutBrief/" + this.props.match.params.id}><p>Schedule In/Brief Out Brief</p></LinkContainer>
+                        </td>
+                        <td>
+                        <LinkContainer to={"/Project/TeamProcessWalk/" + this.props.match.params.id}><p>Team Process Walk</p></LinkContainer>
+                        <LinkContainer to={"/Project/TeamKickOffAwarenessTraining/" + this.props.match.params.id}><p>Team Kick-Off and Awareness Training</p></LinkContainer>
+                        <LinkContainer to={"/Project/ValidateDataCollection/" + this.props.match.params.id}><p>Validate Data Collection</p></LinkContainer>
+                        <LinkContainer to={"/Project/ObtainSupplies/" + this.props.match.params.id}><p>Obtain Supplies</p></LinkContainer>
+                        <LinkContainer to={"/Project/EventGoNoGO/" + this.props.match.params.id}><p>Event Go/No-Go</p></LinkContainer>
+                        <LinkContainer to={"/Project/RoomSetUp/" + this.props.match.params.id}><p>Room Set-Up</p></LinkContainer>
+                    </td>
+                    <td>
+                <LinkContainer to={"/Project/ClairfyValidateProblem/" + this.props.match.params.id}><p>Clarify and Validate the Problem</p></LinkContainer>
+                <LinkContainer to={"/Project/IdentifyPerformanceGaps/" + this.props.match.params.id}><p>Identify Performance Gaps</p></LinkContainer>
+                <LinkContainer to={"/Project/SetImprovementTargets/" + this.props.match.params.id}><p>Set Improvement Target</p></LinkContainer>
+                <LinkContainer to={"/Project/DetermineRootCauses/" + this.props.match.params.id}><p>Determine Root Cause(s)</p></LinkContainer>
+                <LinkContainer to={"/Project/DevelopeCountermeasures/" + this.props.match.params.id}><p>Develop Countermeasures</p></LinkContainer>
+                    </td>
+                    <td>
+                    <LinkContainer to={"/Project/SeeCountermeasuresThrough/" + this.props.match.params.id}><p>See Countermeasures Through</p></LinkContainer>
+                    <LinkContainer to={"/Project/ValidateResults/" + this.props.match.params.id}><p>Validate Results</p></LinkContainer>
+                    </td>
+
+                    <td>
+                        <LinkContainer to={"/Project/StandardizeSuccessfulProcess/" + this.props.match.params.id}><p>Standardize Successful Process</p></LinkContainer>
+                    </td>
+                </tbody>
+                </table>
+            </div>
         );
     }
 }
