8b9ccbc7da37848edf82d7d12c84cdabcf935637
Kaya Garcia <K.Garcia@595.SCS>
2018-10-24 10:42:36 -0500

    Develop Countermeasures progress

--- CPI.Client/ClientApp/src/components/DevelopCountermeasures.js
+++ CPI.Client/ClientApp/src/components/DevelopCountermeasures.js
@@ -10,18 +10,52 @@ export class DevelopCountermeasures extends Component {
     constructor(props, context) {
         super(props, context)
         this.state = {
-            project: {}, loading: true, rootCauses: [], selectedRootCause: {}};
+            project: {}, loading: true, rootCauses: [], newCountermeasures: [] 
+        };
+
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
     }
 
     componentDidMount() {
         fetch('api/Project/GetProjectAsync?id=' + this.props.match.params.id)
             .then(response => response.json())
             .then(data => {
-                this.setState({ project: data, loading: false, rootCauses: data.RootCauses });
+                this.setState({ project: data, loading: false, rootCauses: data.RootCauses, newCountermeasures: [] });
             });
     }
 
-    render(project) {
+    handleAdd(event) {
+        var tempRootCauses = this.state.rootCauses;
+        var tempNewCountermeasures = this.state.newCountermeasures;
+        var indexToUpdate = parseInt(event.target.id);
+
+        tempRootCauses[indexToUpdate].Countermeasures.push(this.state.newCountermeasures[indexToUpdate]);
+        tempNewCountermeasures[indexToUpdate] = "";
+        this.setState({ rootCauses: tempRootCauses, newCountermeasures: tempNewCountermeasures });
+    }
+
+    handleDelete(event) {
+
+    }
+
+    handleChange(event) {
+        var tempCauses = this.state.rootCauses;
+        var tempNewCountermeasures = this.state.newCountermeasures;
+        var changedID = parseInt(event.target.id);
+
+        if (event.target.name === "new") {
+            tempNewCountermeasures[changedID] = event.target.value;
+            this.setState({ newCountermeasures: tempNewCountermeasures }); 
+        } else {
+            tempCauses[parseInt(event.target.id)].Countermeasures[parseInt(event.target.name)] = event.target.value;
+
+            this.setState({ rootCauses: tempCauses });
+        }
+    }
+
+    render() {
         if (this.state.loading) {
             return (<div style={{ left: '50vw', top: '50vh', position: 'absolute' }}>Loading Data</div>);
         } else {
@@ -32,24 +66,32 @@ export class DevelopCountermeasures extends Component {
                         <h3 style={{ marginTop: "10px" }}>Develop countermeaures for each root cause using an appropriate CPI principle</h3>
                     </div>
                     <div className="flexbox">
-                        {this.state.rootCauses.map((Cause, i) => 
-                            (<div>
-                                <label>RC{i + 1}:{Cause.Description}</label>
-                                <table>
-                                <tbody>
-                                    {this.state.rootCauses[i].Countermeasures.map((Measure, j) =>
-                                        (<tr>
-                                            <td>CM{j + 1}</td>
-                                            <td><input id={j} value={Measure} onChange={this.handleChange} /></td>
+                        {/*STINES PLZ FIX*/this.state.rootCauses.map((Cause, i) => (
+                                <div>
+                                    <label>RC{i + 1}:{Cause.Description}</label>
+                                    <table>
+                                            <tbody>
+                                                {Cause.Countermeasures.map((Measure, j) => (
+                                                        <tr>
+                                                            <td>CM{j + 1}</td>
+                                                            <td><input id={i} name={j} value={Measure} onChange={this.handleChange} /></td>
+                                                        </tr>
+                                                            )
+                                                        )
+                                                }
+                                        <tr>
+                                                    <td>{i + 2}</td>
+                                                    <td><input id={i} placeholder="x" name="new" value={this.state.newCountermeasures[i]} onChange={this.handleChange} /></td>
                                         </tr>
-                                    ))}
-                                </tbody>
-                                </table>
+                                            </tbody>
+                                    </table>
+                                <button style={{ float: "right" }} id={i} onClick={this.handleAdd}>Add</button>
                                 </div>
-                            ))}
+                            )
+                        )}
                     </div>
-                    </div>
-    )
-}
-}
+                </div>
+            );
+        }
+    }
 }
