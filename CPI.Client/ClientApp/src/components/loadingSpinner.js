bf986963b6f661958572685264724c3841f9d4eb
Gabriel P. Stines <PagChomp@595.SCS>
2018-10-27 12:23:32 -0500

    getting loading spinner

---
+++ CPI.Client/ClientApp/src/components/loadingSpinner.js
@@ -0,0 +1,12 @@
+﻿import react from 'react';
+import blocKspinner from './img/loading';
+
+export class Spinner extends React.Component {
+
+    render() {
+        <div>
+            <img src={blockSpinner} alt="loading..."/>
+        </div>
+    }
+
+}
